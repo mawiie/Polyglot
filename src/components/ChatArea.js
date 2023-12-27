0dc6de52702e94d1c4f29a0aae19a0a1ce7aa0dd
mawiie <wisdom.ifode@yahoo.com>
2023-12-27 22:46:23 +0900

    Added language choice and input field

--- src/components/ChatArea.js
+++ src/components/ChatArea.js
@@ -2,6 +2,12 @@ import React, {useState} from 'react'
 import ChatBox from './ChatBox'
 
 export default function ChatArea() {
+    const [language, setLanguage] = useState('French')
+
+    function handleClick(e) {
+        setLanguage(e.target.id)
+    }
+
     return (
     <main className='padding'>
         <div className='chat--container'>
@@ -9,10 +15,14 @@ export default function ChatArea() {
             <ChatBox isUser={true}>How are you?</ChatBox>
             <ChatBox isUser={false}>Comment allez-vous?</ChatBox>
             <div className='language--selector'>
-                <button className='language--button' id='Japanese'> </button>
-                <button className='language--button'id='French'></button>
-                <button className='language--button'id='Spanish'></button>
+                <button onClick={(e) => {handleClick(e)}} className='language--button' id='Japanese'> </button>
+                <button onClick={(e) => {handleClick(e)}} className='language--button' id='French'></button>
+                <button onClick={(e) => {handleClick(e)}} className='language--button' id='Spanish'></button>
             </div>
+            <form className='chat--form'>
+                <input className='chat--input' type='text'/>
+                <button className='chat--button' type='submit'></button>
+            </form>
         </div>
     </main>
     )}
