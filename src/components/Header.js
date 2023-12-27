a06d67a8b5652d0558318a6d9276d5ff306dc1b6
mawiie <wisdom.ifode@yahoo.com>
2023-12-27 21:27:38 +0900

    Added Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <img className='header--logo' src='/parrot.png'  alt='Picture of a parrot representing PollyGlot Logo' />
+            <section className='header--text'>
+                <h1 className = 'header--title'>PollyGlot</h1>
+                <p className = 'header--tagline'>Perfect Translation Every Time</p>
+            </section>
+            
+        </header>
+    )
+}
